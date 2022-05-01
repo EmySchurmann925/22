15773cf90db73e86ed0886303b187606ce4f28fe
EmySchurmann925 <97488875+EmySchurmann925@users.noreply.github.com>
2022-04-30 21:00:45 -0400

    Add files via upload

---
+++ PRO-C22-wireframe-main (1)/PRO-C22-wireframe-main/sketch.js
@@ -0,0 +1,39 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world,ground;
+
+
+var bg
+var towerImage
+function preload() {
+ bg=loadImage("assets/background.gif")
+ towerImage=loadImage("assets/tower.png")
+}
+function setup() {
+
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+  
+ options={
+ isStatic:true
+ }
+ 
+ ground= Bodies.rectangle(0,height-1, width*2,1,options);
+ World.add(world,ground);
+ tower= Bodies.rectangle(160,350,160,310,options);
+ World.add(world,tower);
+}
+
+function draw() {
+  background(189);
+  Engine.update(engine);
+ image(bg,0,0,1200,600)
+ rect(ground.position.x, ground.position.y,width*2,1);
+ push ()
+ imageMode(CENTER)
+ image(towerImage,tower.position.x, tower.position.y,160,310)
+ pop ()
+}
